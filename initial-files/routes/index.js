2e45467a1b99e6e76fbf69b0fc1e8acb3520f371
lina mierkE <lina_maria89@hotmail.com>
2021-02-18 17:12:42 -0500

    node errors

--- initial-files/routes/index.js
+++ initial-files/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  console.log('heloooooo!!!!! hi thereee!')
   res.send('Hey! It works!');
+ 
+
 });
 
 module.exports = router;
