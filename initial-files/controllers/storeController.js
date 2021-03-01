4516b33f0850b63e3037919043db48a3c8c40776
lina mierkE <lina_maria89@hotmail.com>
2021-03-01 16:18:44 -0500

    tags set

--- initial-files/controllers/storeController.js
+++ initial-files/controllers/storeController.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
-const multer = require('multer');
-const jimp = require('jimp');
-const uuid = require('uuid')
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
 const multerOptions = {
   storage: multer.memoryStorage(),
-  fileFilter(req, file, next){
-    const isPhoto = file.mimetype.startsWith('image/');
-    if(isPhoto) {
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
       next(null, true);
     } else {
-      next({message: 'That filetype isn\'t allowed'}, false)
+      next({ message: "That filetype isn't allowed" }, false);
     }
-  }
+  },
 };
 
 exports.homePage = (req, res) => {
@@ -23,23 +23,22 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
-exports.upload = multer(multerOptions).single('photo');
+exports.upload = multer(multerOptions).single("photo");
 
 exports.resize = async (req, res, next) => {
   //check if there is not new file to resize
-  if(!req.file){
+  if (!req.file) {
     next(); //skip to the next middleware
     return;
   }
-  const extension = req.file.mimetype.split('/')[1];
+  const extension = req.file.mimetype.split("/")[1];
   req.body.photo = `${uuid.v4()}.${extension}`;
-  //now we resize 
+  //now we resize
   const photo = await jimp.read(req.file.buffer);
   await photo.resize(800, jimp.AUTO);
   await photo.write(`./public/uploads/${req.body.photo}`);
   //once we have written the photo to our filesystem keep going
   next();
-  
 };
 
 exports.createStore = async (req, res) => {
@@ -77,10 +76,12 @@ exports.updateStore = async (req, res) => {
   res.redirect(`/stores/${store._id}/edit`);
 };
 
-
 exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render("store", { store, title: store.name });
+};
 
-  const store = await Store.findOne({ slug: req.params.slug});
-  if(!store) return next();
-  res.render('store', {store, title: store.name})
-}
+exports.getStoreByTag = async (req, res) => {
+  const stores = await Store.getTagsList();
+};
