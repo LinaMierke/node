14c376f17cfe8b6f466f785fb09870af7938138c
lina mierkE <lina_maria89@hotmail.com>
2021-03-09 10:02:20 -0500

    user pw registration

--- initial-files/controllers/userController.js
+++ initial-files/controllers/userController.js
@@ -1,38 +1,50 @@
-const mongoose = require ("mongoose");
-const User = mongoose.model('User');
-const promisify = require('es6-promisify')
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const promisify = require("es6-promisify");
 
 exports.loginForm = (req, res) => {
-    res.render('login', {title: 'Login'})
-}
-
+  res.render("login", { title: "Login" });
+};
 
 exports.registerForm = (req, res) => {
-    res.render('register', { title: 'Register'});
-}
+  res.render("register", { title: "Register" });
+};
 
 exports.validateRegister = (req, res, next) => {
-    req.sanitizeBody('name');
-    req.checkBody('name', 'You must supply a name!').notEmpty();
-    req.checkBody('email', 'Tha Email is not valid!').isEmail();
-    req.sanitizeBody('email').normalizeEmail({
-        remove_dots: false,
-        remove_extension: false,
-        gmail_remove_subaddress: false
-    });
-    req.checkBody('password', 'Password cannot be Blank!').notEmpty();
-    req.checkBody('password-confirm', 'Confirm Password cannot be Blank!').notEmpty();
-    req.checkBody('password-confirm', 'Opps! your password do not match').equals(req.body.password);
+  req.sanitizeBody("name");
+  req.checkBody("name", "You must supply a name!").notEmpty();
+  req.checkBody("email", "Tha Email is not valid!").isEmail();
+  req.sanitizeBody("email").normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false,
+  });
+  req.checkBody("password", "Password cannot be Blank!").notEmpty();
+  req
+    .checkBody("password-confirm", "Confirm Password cannot be Blank!")
+    .notEmpty();
+  req
+    .checkBody("password-confirm", "Opps! your password do not match")
+    .equals(req.body.password);
 
-    const errors = req.validationErrors();
-    if(errors) {
-        req.flash('error', errors.map(err => err.msg));
-        res.render('register', {title: 'Register', body: req.body, flashes: req.flash()});
-        return; // stop the function from running
-    }
-    next(); //there were no errors
-}
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash(
+      "error",
+      errors.map((err) => err.msg)
+    );
+    res.render("register", {
+      title: "Register",
+      body: req.body,
+      flashes: req.flash(),
+    });
+    return; // stop the function from running
+  }
+  next(); //there were no errors
+};
 
 exports.register = async (req, res, next) => {
-
+    const user = new User({email: req.body.email, name: req.body.name })
+    const register = promisify(User.register, User);
+    await register(user, req.body.password);
 }
